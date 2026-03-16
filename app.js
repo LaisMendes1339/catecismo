840a877243e53f2de70abe33b264e1a3f5f0d50a
LaisMendes1339 <laismendesgeo98@gmail.com>
2026-03-16 12:22:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ import {
   orderBy,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 /* =========================================================
    CONFIG
 ========================================================= */
